56f9ffdcd82e54acf3c13f74755137de37266f82
xLeDocteurx <lenoirc.fr@gmail.com>
2018-10-09 22:07:59 +0200

    pretty littles buttons on clips

--- src/components/parts/Clip.js
+++ src/components/parts/Clip.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { 
+    Button,
     // Image, Dropdown, Menu, 
     // Divider
     } from 'semantic-ui-react'
@@ -32,18 +33,16 @@ export default class Clip extends Component {
         // let saveProjects = DB.saveProjects();
 
         return (
-            <div>
-                <div className="btn-group w-100" role="group" aria-label="Basic example">
-                    <button type="button" className="rounded-0 border btn btn-sm w-100" 
-                        // onclick="request_clip({this.props.track.name}, {this.props.clip.id})"
-                    >{this.props.clip.name}</button>
-                    <button type="button" className="rounded-0 border btn btn-sm launch-btn" 
-                        // onclick="launch_clip(<%= track.id %>, <%= index %>);"
-                    >
-                        <i className="fas fa-play"></i>
-                    </button>
-                </div>
-            </div>
+            <Button.Group fluid className="btn-group w-100" role="group" aria-label="Basic example">
+                <Button type="button" className="rounded-0 border btn btn-sm w-100" 
+                    // onclick="request_clip({this.props.track.name}, {this.props.clip.id})"
+                >{this.props.clip.name}</Button>
+                <Button icon type="button" className="rounded-0 border btn btn-sm launch-btn" 
+                    // onclick="launch_clip(<%= track.id %>, <%= index %>);"
+                >
+                    <i className="fas fa-play"></i>
+                </Button>
+            </Button.Group>
         )
     }
 }
